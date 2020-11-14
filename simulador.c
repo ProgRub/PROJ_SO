821a7cee674be24fb94b9b7140075521a49133ae
ProgRub <ruben2000jgr@gmail.com>
2020-11-14 17:12:05 +0000

    Comentario das funcoes para sockets

--- simulador.c
+++ simulador.c
@@ -21,6 +21,137 @@ struct Configuration
     int tempoSimulacao;
 };
 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <sys/types.h>
+// #include <sys/socket.h>
+// #include <sys/un.h>
+
+// #define UNIXSTR_PATH "/tmp/2046018"
+// #define UNIXDG_PATH  "/tmp/s.unixdfddffdfdgx"
+// #define UNIXDG_TMP   "/tmp/dgXXXXXfdjhfdjhfdXX"
+
+
+// /* Cliente do tipo socket stream.
+//    L▒ linhas do teclado e envia-as para o servidor */
+
+// #include "unix.h"
+
+// main(void)
+// {
+//         int sockfd, servlen;
+//         struct sockaddr_un serv_addr;
+
+//         /* Cria socket stream */
+
+//         if ((sockfd= socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
+//                 err_dump("client: can't open stream socket");
+
+//         /* Primeiro uma limpeza preventiva!
+//            Dados para o socket stream: tipo + nome do ficheiro.
+//          O ficheiro identifica o servidor */
+
+//         bzero((char *) &serv_addr, sizeof(serv_addr));
+//         serv_addr.sun_family = AF_UNIX;
+//         strcpy(serv_addr.sun_path, UNIXSTR_PATH);
+//         servlen = strlen(serv_addr.sun_path) + sizeof(serv_addr.sun_family);
+
+//         /* Tenta estabelecer uma liga▒▒o. S^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^$
+//          sido lan▒ado primeiro (o servidor tem de criar o ficheiro e associar
+//          o socket ao ficheiro) */
+
+//         if (connect(sockfd, (struct sockaddr *) &serv_addr, servlen) < 0)
+//                 err_dump("client: can't connect to server");
+
+//         /* Envia as linhas lidas do teclado para o socket */
+
+//         str_cli(stdin, sockfd);
+
+//         /* Fecha o socket e termina */
+
+//         close(sockfd);
+//         exit(0);
+// }
+
+
+/* Servidor do tipo socket stream.
+   Recebe linhas do cliente e reenvia-as para o cliente */
+
+// #include "unix.h"
+
+// main(void)
+// {
+//         int sockfd, newsockfd, clilen, childpid, servlen;
+//         struct sockaddr_un cli_addr, serv_addr;
+
+//         /* Cria socket stream */
+
+//         if ((sockfd = socket(AF_UNIX,SOCK_STREAM,0)) < 0)
+//                 err_dump("server: can't open stream socket");
+
+//         /* Primeiro uma limpeza preventiva!
+//            Dados para o socket stream: tipo + nome do ficheiro.
+//          O ficheiro serve para que os clientes possam identificar o servidor */
+
+//         bzero((char *)&serv_addr, sizeof(serv_addr));
+//         serv_addr.sun_family = AF_UNIX;
+//         strcpy(serv_addr.sun_path, UNIXSTR_PATH);
+
+//       /* O servidor ▒ quem cria o ficheiro que identifica o socket.
+//          Elimina o ficheiro, para o caso de algo ter ficado pendurado.
+//          Em seguida associa o socket ao ficheiro.
+//          A dimens▒o a indicar ao bind n▒o ▒ a da estrutura, pois depende
+//          do nome do ficheiro */
+
+//         servlen = strlen(serv_addr.sun_path) + sizeof(serv_addr.sun_family);
+//         unlink(UNIXSTR_PATH);
+//         if (bind(sockfd, (struct sockaddr *) &serv_addr, servlen) < 0)
+//                 err_dump("server, can't bind local address");
+
+//         /* Servidor pronto a aceitar 5 clientes para o socket stream */
+
+//         listen(sockfd, 5);
+
+//         for (;;) {
+
+//                 /* N▒o esquecer que quando o servidor aceita um cliente cria um
+//                    socket para comunicar com ele. O primeiro socket (sockfd) fica
+//                    ▒ espera de mais clientes */
+
+//                 clilen = sizeof(cli_addr);
+//                 newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr,
+//                                    &clilen);
+//                 if (newsockfd < 0)
+//                         err_dump("server: accept error");
+
+//                 /* Lan▒a processo filho para lidar com o cliente */
+
+//                 if ((childpid = fork()) < 0)
+//                         err_dump("server: fork error");
+
+//                 else if (childpid == 0) {
+
+//                         /* Processo filho que vai atender o cliente.
+//                            Fechar sockfd ▒ sanit▒rio, j▒ que n▒o ▒
+//                            utilizado pelo processo filho.
+//                            Os dados recebidos do cliente s▒o reenviados
+//                            para o cliente */
+
+//                         close(sockfd);
+//                         str_echo(newsockfd);
+//                         exit(0);
+//                 }
+
+//                 /* Processo pai.
+//                    Fechar newsockfd ▒ sanit▒rio, j▒ que n▒o ▒
+//                    utilizado pelo processo pai */
+
+//                 close(newsockfd);
+//         }
+// }
+
+
 int main(int argc, char *argv[])
 {
     if (argc == 2)
