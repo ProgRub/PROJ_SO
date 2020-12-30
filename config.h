13fa54ca3132abea984bc1b4bbde171692a95e39
orlandoxnica <58520368+orlandoxnica@users.noreply.github.com>
2020-12-30 16:47:42 +0000

    Update config.h
    Cores

--- config.h
+++ config.h
@@ -20,6 +20,17 @@
 #define TESTE_NORMAL 0
 #define TESTE_RAPIDO 1
 
+//CORES
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
 //ESTADO TESTE
 #define NAO_TESTOU 0
 #define POSITIVO 1
